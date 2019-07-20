5a65c2942886203b05d6945adde2f7da573fd3b9
JP314 <jan.grau95@gmail.com>
2019-07-20 20:36:07 +0200

    Extract typescript strings.

--- Strings.Typescript/app.ts
+++ Strings.Typescript/app.ts
@@ -1,32 +1,103 @@
-﻿import * as ts from "typescript";
+import * as ts from "typescript";
+import * as fs from "fs";
 
-let sourceFile = ts.createSourceFile("hello.ts", `
-console.log("Hello World");
-console.log('Hello World');
-console.log(\`asd\${11}\`);
-`, ts.ScriptTarget.Latest, true);
+async function main(args: string[]) {
 
-// https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#traversing-the-ast-with-a-little-linter
-
-let stringNodes = [];
-visitNode(sourceFile);
-while (true) { }
-
-function visitNode(node: ts.Node) {
-	switch (node.kind) {
-		case ts.SyntaxKind.StringLiteral:
-			report(node, "string literal");
-			break;
-
-		case ts.SyntaxKind.TemplateExpression:
-			report(node, "string template");
+	if (args.length !== 2) {
+		console.error("Expected exactly two arguments: <input-file> <output-file>");
+		process.exit(1);
 	}
 
-	ts.forEachChild(node, visitNode);
+	const fileOptions = { encoding: "utf8" };
+
+	const [inputListPath, outputPath] = args;
+
+	const inputFiles = fs.readFileSync(inputListPath, fileOptions).trim().split(/[\r\n]+/g)
+
+	const stream = fs.createWriteStream(outputPath, fileOptions);
+	for (const inputPath of inputFiles) {
+		const sourceText = fs.readFileSync(inputPath, fileOptions);
+		const source = ts.createSourceFile(inputPath, sourceText, ts.ScriptTarget.Latest, true);
+		const searchResult = extractStringLiterals(source);
+		const text = searchResult.map((item) => item.toString()).join("\r\n");
+		await new Promise((resolve) => stream.write(text, resolve));
+	}
+	stream.close();
 }
 
-function report(node: ts.Node, message: string) {
-	const { line, character } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
-	console.log(`${sourceFile.fileName} (${line + 1},${character + 1}): ${message} | ` + ts.SyntaxKind[node.kind] + " | " + node.getFullText());
-	stringNodes.push(node.getFullText());
+function extractStringLiterals(source: ts.SourceFile) {
+	const result: SearchResult[] = [];
+	visitNode(source);
+	return result;
+
+	function visitNode(node: ts.Node) {
+		switch (node.kind) {
+			case ts.SyntaxKind.StringLiteral:
+				report(node);
+				break;
+
+			case ts.SyntaxKind.TemplateExpression:
+				report(node);
+		}
+
+		ts.forEachChild(node, visitNode);
+	}
+
+	function report(node: ts.Node) {
+		const item = new SearchResult();
+		item.path = source.fileName;
+		item.startIndex = node.getFullStart();
+		item.endIndex = item.startIndex + node.getFullWidth();
+		const { line, character } = source.getLineAndCharacterOfPosition(item.startIndex);
+		item.line = line;
+		item.character = character;
+		item.text = node.getFullText();
+		item.source1 = "typescript";
+		item.source2 = ts.SyntaxKind[node.kind];
+		item.source3 = "";
+		result.push(item);
+	}
 }
+
+class SearchResult {
+
+	public path: string;
+	public startIndex: number;
+	public endIndex: number;
+	public line: number;
+	public character: number;
+	public text: string;
+	public source1: string;
+	public source2: string;
+	public source3: string;
+
+	public toString() {
+
+		let result = "";
+		writeLengthEncoded(this.path);
+		writeCell(this.startIndex);
+		writeCell(this.endIndex);
+		writeCell(this.line);
+		writeCell(this.character);
+		writeLengthEncoded(this.source1);
+		writeLengthEncoded(this.source2);
+		writeLengthEncoded(this.source3);
+		writeLengthEncoded(this.text);
+		return result;
+
+		function writeLengthEncoded(value: string) {
+			value = value || "";
+			writeCell(value.length);
+			writeCell(value);
+		}
+
+		function writeCell(value: any) {
+			result += value + ";";
+		}
+	}
+}
+
+main(process.argv.slice(2)).catch((err) => {
+	console.error(err);
+	process.exit(4);
+});
